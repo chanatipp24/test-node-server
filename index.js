681b8f608623294dfe9a88d7d7d5f29bf9b66836
Ken <chanatip_har@truecorp.co.th>
2023-02-14 10:57:58 +0700

    test node cron

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
+const cron = require('node-cron')
 
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
     res.json("Hello, World")
 })
+
+cron.schedule('* * * * *', (now) => {
+    console.log('Run task every minute ' + now);
+});
 
 app.listen(PORT, () => {
     console.log("Server running port " + PORT);
